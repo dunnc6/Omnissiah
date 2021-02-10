1f9ce51ee10e018bdd362f69c62af0be140eff24
Calvin Dunn <dunnc6@wwu.edu>
2021-02-10 14:26:03 -0800

    Revisions

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
-public class HelloWorld{
-    public static void main(String []args) {
+public class HelloWorld{   //<----- This is the name of the file
+    public static void main(String []args) {  //<---- This is the main function of the file
 
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //<----- This is a print line. It will print Hello World to the user via the println function.
 
     }
 
